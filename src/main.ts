5c7a19f32bbc1cbc2f1c36f86619675a2b9d2aeb
rtorres <rene.torres@csps-efpc.gc.ca>
2023-04-04 15:12:43 -0400

    load styles

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
+import './assets/scss/styles.scss'
 // import router from './router'
 
 const app = createApp(App)
